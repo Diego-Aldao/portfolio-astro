64d69aa28f5638ef98c326e5c60bb784853f9ded
Diego Aldao <diegofrontdev@gmail.com>
2024-03-04 22:52:41 -0300

    agregados los colores custom

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"main-black": "#101010",
+				"main-white": "#f2f2f2",
+				"secondary-white": "#afafaf",
+			}
+		},
 	},
 	plugins: [],
 }
